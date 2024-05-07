7a3e1241c8e4a3b0bc849ce6e36dc0835bf18e21
David Alda <aldazgz95@gmail.com>
2024-05-07 21:18:02 +0200

    David mi nombre

---
+++ src/david/David.java
@@ -0,0 +1,15 @@
+package david;
+
+public class David {
+
+	public static void main(String[] args) {
+		miNombre();
+	}
+
+	
+	private static void miNombre() {
+		System.out.println("Soy DAVID");
+	} 
+		
+	
+}
